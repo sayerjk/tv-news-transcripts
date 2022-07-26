531b2f2ca27ecb7ce8d3d1c8addd23202f53f584
sayerjk <sayerjk@gmail.com>
2022-07-26 11:56:21 -0700

    removed test functions and comments

--- news-audit/main.py
+++ news-audit/main.py
@@ -2,15 +2,11 @@ from transcripts import get_transcript, get_transcript_links, segment_links
 from data import send_to_mongo
 
 show_index = 'https://transcripts.cnn.com/show/acd'
-batch = [get_transcript(item) for item in get_transcript_links(show_index)[:3]]
-# print(f'\nSample entry: {batch[0]}')
+batch = [get_transcript(item) for item in get_transcript_links(show_index, 1)]
+print(f'\nSample entry: {batch[0]}')
 
 front_page = 'https://transcripts.cnn.com/'
 idx = segment_links(front_page)
-# print(f'All shows: {idx}')
-
-db_name = 'transcripts'
-col_name = 'sentiment_textblob'
-send_to_mongo(db_name, col_name, batch[0])
+print(f'All shows: {idx}')
 
 print('Done')
